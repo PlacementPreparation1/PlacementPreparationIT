d5b55e4d83f6d2315e4ca993ec86d75ef499b56b
PlacementPreparation1 <placementpreparationguidance@gmail.com>
2026-03-01 20:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function goToRegister() {
+window.location.href = "register.html";
+}
+
+function registerUser() {
+alert("Registration Successful! (Demo Version)");
+return false;
+}
+
+function loginUser() {
+alert("Login Successful! (Demo Version)");
+return false;
+}
+
+function resetPassword() {
+alert("Password Reset Link Sent! (Demo)");
+return false;
+}
